713b52be3c4b3ea04cf9eb2dd0f7ba2e5336a781
iethree <iethree@gmail.com>
2021-04-19 12:09:14 -0500

    add stats view

--- bgt.js
+++ bgt.js
@@ -1,6 +1,10 @@
 // js
 let records = [];
-const display_format = "h:mm a";
+let summary = {};
+let mode = localStorage.mode || 'record';
+
+const TIME_FORMAT = "h:mm a";
+const DATE_FORMAT = 'dddd MMMM D';
 const icons = {
   'left': `<span class="text-green-500"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline" viewBox="0 0 20 20" fill="currentColor">
   <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clip-rule="evenodd" />
@@ -9,22 +13,27 @@ const icons = {
   <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clip-rule="evenodd" />
 </svg></span>`,
   'poop': '💩',
-  'pee': `<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline" viewBox="0 0 20 20" fill="currentColor">
-  <path fill-rule="evenodd" d="M7 2a1 1 0 00-.707 1.707L7 4.414v3.758a1 1 0 01-.293.707l-4 4C.817 14.769 2.156 18 4.828 18h10.343c2.673 0 4.012-3.231 2.122-5.121l-4-4A1 1 0 0113 8.172V4.414l.707-.707A1 1 0 0013 2H7zm2 6.172V4h2v4.172a3 3 0 00.879 2.12l1.027 1.028a4 4 0 00-2.171.102l-.47.156a4 4 0 01-2.53 0l-.563-.187a1.993 1.993 0 00-.114-.035l1.063-1.063A3 3 0 009 8.172z" clip-rule="evenodd" />
-</svg>`,
+  'pee': `<span class="text-yellow-500"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+  class="w-4 h-4 inline" viewBox="0 0 572.8 572.801" style="enable-background:new 0 0 572.8 572.801;"
+  fill="currentColor"
+  xml:space="preserve">
+<path d="M520.25,353.6c0,125-100.5,219.201-233.8,219.201S52.55,478.6,52.55,353.6c0-115.3,164.7-301.5,197.7-337.7
+   c9.3-10.2,22.4-15.9,36.2-15.9s26.9,5.8,36.2,15.9C355.55,52.1,520.25,238.4,520.25,353.6z"/>
+</svg> </span>`,
 };
 
 load();
-drawRecords();
 updateTime();
 setInterval(updateTime, 1000 * 5);
+draw(mode);
 
 
-document.querySelectorAll('button').forEach(el=>el.addEventListener('click', record))
+document.querySelectorAll('button.button').forEach(el=>el.addEventListener('click', record));
+document.querySelector('#mode-switch').addEventListener('click', switchMode);
+
 
 // store data locally: no data transmission
 function record(el) {
-
   const type = el?.currentTarget?.dataset?.type;
   if (!type) {
     return;
@@ -32,7 +41,7 @@ function record(el) {
 
   records.unshift({id: uuid(), type, time: new Date().toISOString() });
   saveRecords();
-  drawRecords();
+  draw(mode);
 }
 
 function saveRecords(){
@@ -40,11 +49,41 @@ function saveRecords(){
   localStorage.setItem('data', JSON.stringify(records));
 }
 
+function getSummary(){
+  const summary = {};
+  records.forEach(r => {
+    const dateString = dayjs(r.time).format('YYYYMMDD');
+    if (!summary[dateString]) {
+      summary[dateString] = {poop: 0, pee: 0, left: 0, right: 0};
+    }
+    summary[dateString][r.type]++;
+
+  });
+  return summary;
+}
+
 // load data from local device, if any exists
 function load() {
   if (localStorage.data) {
     records = JSON.parse(localStorage.data);
   }
+}
+
+
+function switchMode(e) {
+  mode = mode === 'summary' ? 'records' : 'summary';
+  localStorage.setItem('mode', mode);
+
+  if (mode === 'summary') {
+    document.getElementById('mode-switch').classList.remove('text-gray-700');
+  } else {
+    document.getElementById('mode-switch').classList.add('text-gray-700');
+  }
+  draw(mode);
+}
+
+function draw(mode) {
+  mode === 'summary' ? drawSummary() : drawRecords();
 }
 
 function drawRecords() {
@@ -65,6 +104,36 @@ function drawRecords() {
   drawTimeSinceLastEvent();
 }
 
+function drawSummary() {
+  const days = getSummary();
+  const dates = Object.keys(days).sort().reverse();
+
+  const table = dates?.length
+    ? `<table class="striped w-full text-center text-lg">${dates.map(d=>drawStats(d, days[d])).join('')}</table>`
+    : '<div class="p-10">No Records</div>';
+
+  document.getElementById('data-table').innerHTML = table;
+}
+
+function drawStats(date, data) {
+  return `
+  <tr>
+    <td colspan="2" class="pt-3 pb-1 text-sm text-gray-400">${dayjs(date).format(DATE_FORMAT)}</td>
+  </tr>
+  <tr>
+    <td>${icons.left} / ${icons.right}</td>
+    <td>${data.left} / ${data.right}</td>
+  </tr>
+  <tr>
+    <td>${icons.poop}</td>
+    <td>${data.poop}</td>
+  </tr>
+  <tr>
+    <td>${icons.pee}</td>
+    <td>${data.pee}</td>
+  </tr>`;
+}
+
 function drawRecord(record, index) {
   if(!record?.type || !record?.time) return '';
 
@@ -72,19 +141,20 @@ function drawRecord(record, index) {
   if (index === 0 || dayjs(records[index-1].time).format('YYYYMMDD') != dayjs(record.time).format('YYYYMMDD')) {
     separator = `</table><table class="striped w-full text-center">
       <tr>
-        <td colspan="2" class="bg-gray-700 pt-3 pb-1 text-sm text-gray-500">${dayjs(record.time).format('dddd MMMM D')}
+        <td colspan="2" class="bg-gray-700 pt-3 pb-1 text-sm text-gray-400">${dayjs(record.time).format(DATE_FORMAT)}
         </td>
       </tr>`;
   }
 
   return `${separator}
-  <tr data-id="${record.id}" 
-    class="selectable-row mb-2 text-lg transition-all transition transform origin-bottom transform ${index === 0 ? 'scale-0' : '-translate-y-4'}" 
-    onclick="editModal(${index})"
-  >
-    <td class="px-5">${icons[record.type]}</td>
-    <td>${dayjs(record.time).format(display_format)}</td>
-  </tr>`;
+    <tr data-id="${record.id}" 
+      class="selectable-row mb-2 text-lg transition transform origin-bottom ${index === 0 ? 'scale-0' : '-translate-y-4'}" 
+      onclick="editModal(${index})"
+    >
+      <td class="px-5">${icons[record.type]}</td>
+      <td>${dayjs(record.time).format(TIME_FORMAT)}</td>
+    </tr>`;
+
 }
 
 function drawTimeSinceLastEvent(){
